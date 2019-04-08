657476ca6d759feb3341482bfd3d8682c7f1c698
Jenifer Martin <jenifer.martin@vml.com>
2019-04-08 15:28:48 -0500

    fixed bug

--- app/src/main/java/com/example/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/smack/Controller/MainActivity.kt
@@ -18,6 +18,7 @@ import com.example.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
+import kotlinx.android.synthetic.main.nav_header_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,32 +28,31 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
-        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver,
-            IntentFilter(BROADCAST_USER_DATA_CHANGE))
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+            userDataChangeReceiver,
+            IntentFilter(BROADCAST_USER_DATA_CHANGE)
+        )
     }
 
-    override fun onRestart() {
-        super.onRestart()
-        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver,
-            IntentFilter(BROADCAST_USER_DATA_CHANGE))
-    }
-
-    private val userDataChangeReceiver = object: BroadcastReceiver() {
+    private val userDataChangeReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
-            setContentView(R.layout.nav_header_main)
+            //setContentView(R.layout.nav_header_main)
 
             if (AuthService.isLoggedIn) {
-                userNameNavHeader.text = UserDataService.name
-                userEmailNavHeader.text = UserDataService.email
-                val resourceId = resources.getIdentifier(UserDataService.avatarName, "drawable",
-                    packageName)
-                userImageNavHeader.setImageResource(resourceId)
-                userImageNavHeader.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
-                loginButtonNavHeader.text = "Logout"
+                nav_drawer_header_include.userNameNavHeader.text = UserDataService.name
+                nav_drawer_header_include.userEmailNavHeader.text = UserDataService.email
+                val resourceId = resources.getIdentifier(
+                    UserDataService.avatarName, "drawable",
+                    packageName
+                )
+                nav_drawer_header_include.userImageNavHeader.setImageResource(resourceId)
+                nav_drawer_header_include.userImageNavHeader.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
+                nav_drawer_header_include.loginButtonNavHeader.text = "Logout"
             }
         }
     }
@@ -70,16 +70,17 @@ class MainActivity : AppCompatActivity() {
         if (AuthService.isLoggedIn) {
 
             UserDataService.logout()
-            userNameNavHeader.text = ""
-            userEmailNavHeader.text = ""
-            userImageNavHeader.setImageResource(R.drawable.profiledefault)
-            userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
-            loginButtonNavHeader.text = "Login"
+            nav_drawer_header_include.userNameNavHeader.text = ""
+            nav_drawer_header_include.userEmailNavHeader.text = ""
+            nav_drawer_header_include.userImageNavHeader.setImageResource(R.drawable.profiledefault)
+            nav_drawer_header_include.userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
+            nav_drawer_header_include.loginButtonNavHeader.text = "Login"
 
         } else {
             val loginIntent = Intent(this, LoginActivity::class.java)
             startActivity(loginIntent)
         }
+
     }
 
     fun addChannelClicked(view: View) {
